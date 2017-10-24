7264d466d5e4259cc9b1c91d9ae853dcb7d56a78
beyondamos <328122186@qq.com>
2017-10-24 17:11:50 +0800

    优化了首页轮播代码

--- views/index/index.php
+++ views/index/index.php
@@ -11,43 +11,53 @@ use yii\widgets\LinkPager;
                         <div class="col-sm-6">
                             <div id="carousel-banner" class="carousel slide banner-slide" data-ride="carousel">
                               <!-- Indicators -->
-                              <ol class="carousel-indicators">
-                                <li data-target="#carousel-banner" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-banner" data-slide-to="1"></li>
-                                <li data-target="#carousel-banner" data-slide-to="2"></li>
-                            </ol>
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-banner" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-banner" data-slide-to="1"></li>
+                                    <li data-target="#carousel-banner" data-slide-to="2"></li>
+                                </ol>
                             <!-- Wrapper for slides -->
-                            <div class="carousel-inner" role="listbox">
-                                <div class="item active" style="background-image:url('/home/images/166.jpg')"></div>
-                                <div class="item" style="background-image:url('/home/images/87341-1.jpg')"></div>
-                                <div class="item" style="background-image:url('/home/images/87349-1.jpg')"></div>
-                                <div class="item" style="background-image:url('/home/images/87392-1.jpg')"></div>
-                                <div class="item" style="background-image:url('/home/images/87444-1.jpg')"></div>
-                            </div>
+                                <div class="carousel-inner" role="listbox">
+                                    <div class="item active">
+                                        <img src="/home/images/87392-1.jpg">
+                                        <div class="carousel-caption">
+                                            没有明星代言，小公司该如何推
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="/home/images/87341-1.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img src="/home/images/87349-1.jpg">
+                                    </div>
+    <!--                                 <div class="item" style="background-image:url('/home/images/87392-1.jpg')"></div>
+                                    <div class="item" style="background-image:url('/home/images/87444-1.jpg')"></div> -->
+                                </div>
 
-                            <!-- Controls -->
-                            <a class="left carousel-control" href="#carousel-banner" role="button" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-banner" role="button" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div></div>
+                                <!-- Controls -->
+                                <a class="left carousel-control" href="#carousel-banner" role="button" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="right carousel-control" href="#carousel-banner" role="button" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>  
+                        </div>
                         <div class="col-sm-6">
                             <ul class="banner-r">
                                 <li>
                                     <h3><a href="">没有明星代言，小公司该如何推广自己的APP?小公司该如何推广自己的APP?</a></h3>
-                                    <p>明星代言APP，其实只是一个噱头，真正的制胜关键，是将APP的用户运营好，将用户转化为忠</p>
+                                    <p>明星代言APP，其实只是一个噱头，真正的制胜关键</p>
                                 </li>
                                 <li>
                                     <h3><a href="">从下岗女工到集团老总，从8平米的小店到市值</a></h3>
-                                    <p>她的一生很不走运：读初中时遇上文化大革命，被下放到农村当了5年农民；好不容易回了城，有了一份固定工作，却</p>
+                                    <p>她的一生很不走运：读初中时遇上文化大革命，</p>
                                 </li>
                                 <li>
                                     <h3><a href="">电子毒品《王者荣耀》到底毒在哪</a></h3>
-                                    <p>《王者荣耀》系统又双叒更新了，新赛季的系统以夏侯淳、小乔和钟无艳的夏日皮肤为主题...</p>
+                                    <p>《王者荣耀》系统又双叒更新了，新赛季的系统以夏侯</p>
                                 </li>
                             </ul></div>
                         </div>
